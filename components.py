4888fc8e6ce79a922c57244592c09fa72c6bd7ec
ddjj8472 <ddjj0911@gmail.com>
2026-04-29 14:36:10 +0900

    Update components.py

--- components.py
+++ components.py
@@ -1,13 +1,8 @@
 import streamlit as st
 
 def render_user_message(query):
-    st.markdown(
-        f'<div class="user-msg">질문: {query}</div>',
-        unsafe_allow_html=True
-    )
+    st.markdown(f'<div class="user-msg">질문: {query}</div>', unsafe_allow_html=True)
 
 def render_ai_report(response_text):
-    st.markdown(
-        f'<div class="report-card">{response_text}</div>',
-        unsafe_allow_html=True
-    )
+    formatted_text = response_text.replace("\n", "<br>")
+    st.markdown(f'<div class="report-card">{formatted_text}</div>', unsafe_allow_html=True)
