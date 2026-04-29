3e4de06df393fd4c82cfb2d12e4dc181dc4f2428
ddjj8472 <ddjj0911@gmail.com>
2026-04-29 15:26:30 +0900

    Update components.py

--- components.py
+++ components.py
@@ -5,6 +5,7 @@ def render_user_message(query):
     st.markdown(f'<div class="user-msg">질문: {query}</div>', unsafe_allow_html=True)
 
 def render_ai_report(response_text):
+    #6가지 항목 구성(존재할시)볼드체로
     titles = ["결론", "적용 지역", "핵심 근거", "세부 해석", "원문 링크", "담당 기관"]
 
     for title in titles:
